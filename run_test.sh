fa11db6adaf823e33324307c4b76cd3384d11bda
Tom Krueger <83777191+tomkrueger42@users.noreply.github.com>
2023-02-27 20:47:35 +0100

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -3,5 +3,5 @@
 ./std_containers > out.std_containers
 ./ft_containers > out.ft_containers
 
-diff out.std_containers out.ft_containers > out.std_ft.diff
+diff out.std_containers out.ft_containers > out.results.diff
 diff out.std_containers out.ft_containers
